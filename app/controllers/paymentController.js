40eb186e2b6532d7a460954a61f368e979701728
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2023-12-11 12:08:06 +0300

    refactor: using env variable

--- app/controllers/paymentController.js
+++ app/controllers/paymentController.js
@@ -7,12 +7,12 @@ exports.makePayment = async (req, res) => {
   try {
     if (partyA && partyB && amount) {
       await paymentService.processPayment(req.body);
-      res.status(200).json({ message: "Payment successful" });
+      res.status(200).json("Payment successful");
     } else {
-      res.status(422).json({ message: "Invalid request data" });
+      res.status(422).json("Invalid request data");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    res.status(500).json("Internal Server Error" );
   }
 };
--- app/services/emailService.js
+++ app/services/emailService.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const {
   studentPlatformRegistrationNotification,
   studentCourseEnrollmentNotification,
